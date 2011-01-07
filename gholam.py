504c570d91ca4ae23ce1d0254c3c425ea9c7095c
Amin Oruji <aminpy@gmail.com>
2011-01-07 17:23:05 +0330

    appending license

--- gholam.py
+++ gholam.py
@@ -1,4 +1,16 @@
 #coding: utf-8
+##   Copyright (C) 2010-2011 Amin Oruji (aminpy@gmail.com)
+##
+##   This program is free software; you can redistribute it and/or modify
+##   it under the terms of the GNU General Public License as published by
+##   the Free Software Foundation; either version 2, or (at your option)
+##   any later version.
+##
+##   This program is distributed in the hope that it will be useful,
+##   but WITHOUT ANY WARRANTY; without even the implied warranty of
+##   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##   GNU General Public License for more details.
+
 import socket
 import foshes
 from xgoogle.translate import Translator
