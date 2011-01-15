c164fe87778e2169a2f2b14df67159da38f089b3
Amin Oruji <aminpy@gmail.com>
2011-01-15 16:13:01 +0330

    add media

--- settings.py
+++ settings.py
@@ -45,17 +45,17 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/home/amin/workspace/gholam/gholam/statics'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = 'media'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/mediaAdmin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'a6sejy0xi@ab3jt&j+2@3pwzc$y0iqdt_%hzy-vh7xlqru3(w('
