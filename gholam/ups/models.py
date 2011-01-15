2f1bbbb6aaaf4732c919e5d333c472f7a47b2fbb
Amin Oruji <aminpy@gmail.com>
2011-01-15 12:29:51 +0330

    some corrections

--- gholam/ups/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class UserPass(models.Model):
-    username = models.CharField(max_length=32)
-    password = models.CharField(max_length=32)
-    
-    def __unicode__(self):
-        return self.username + " " + self.password
