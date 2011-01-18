7eb82a2e298c351e535c02801fb4c1ae85c05e34
Amin Oruji <aminpy@gmail.com>
2011-01-18 12:46:19 +0330

    some optimizations

--- gholam.py
+++ gholam.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 ##   Copyright (C) 2010-2011 Amin Oruji (aminpy@gmail.com)
 ##
 ##   This program is free software; you can redistribute it and/or modify
@@ -15,6 +14,7 @@ from twisted.words.protocols import irc
 from translate.translate import Translator
 
 class Gholam(irc.IRCClient):
+    isChannel = False
 
     def _get_nickname(self):
         return self.factory.nickname
@@ -26,46 +26,56 @@ class Gholam(irc.IRCClient):
         print "Ba id e %s be server vasl shodam." % (self.nickname,)
 
     def joined(self, channel):
+        self.isChannel = True
         print "Raftam tu %s." % (channel,)
+        
+        
+    def userRenamed(self, oldname, newname): 
+        self.msg("#harchi", "%s, shakh shodi nick avaz mikoni?!" % newname)
 
     def privmsg(self, user, channel, msg):
-
-        if channel[0] == "#":
+        if self.isChannel:
             id = user.split("!")[0]
-            ebarat = msg[5:]
-            send = ""
-            print "%s: %s" % (id, msg,)
-
-            if msg == "!help":
-                send = "%s, https://bitbucket.org/aminpy/gholam/issue/1/gholam-commands" % id
-                self.msg(channel, send)
-                
-            elif msg == "!about":
-                send = "%s, My name is Gholam, I was born in 28 December 2010 and I'm written in python." % id
-                self.msg(channel, send)
-
-            elif msg.startswith(".web "):
-                url = "http://www.google.com/search?q=%s" % ebarat
-                send = "%s, %s" % (id, url.replace(" ", "+"))
-                self.msg(channel, send)
-
-            elif msg.startswith(".img "):
-                url = "http://www.google.com/images?q=%s" % ebarat
-                send = "%s, %s" % (id, url.replace(" ", "+"))
-                self.msg(channel, send)
-
-            elif msg.startswith(".vid "):
-                url = "http://www.google.com/search?q=%s&tbs=vid:1" % ebarat
-                send = "%s, %s" % (id, url.replace(" ", "+"))
-                self.msg(channel, send)
-                
-            elif msg.startswith(".") and msg[5] == " ":
-                msg = msg.split(" ")[0]
-                lt = msg[1:3]
-                lf = msg[-2:]
-                matn = Translator().translate(ebarat, lf, lt).encode("utf-8")
-                send = "%s, %s" % (id, matn)
-                self.msg(channel, send)
-
-            if send:
-                print "%s: %s" % (self.nickname, send)
+            if channel[0] == "#":
+                ebarat = msg[5:]
+                send = ""
+                print "%s: %s" % (id, msg,)
+    
+                if msg == "!help":
+                    send = "%s, https://bitbucket.org/aminpy/gholam/issue/1/gholam-commands" % id
+                    self.msg(channel, send)
+                    
+                elif msg == "!about":
+                    send = "%s, My name is Gholam, I was born in 28 December 2010 and I'm written in python." % id
+                    self.msg(channel, send)
+    
+                elif msg.startswith(".web "):
+                    url = "http://www.google.com/search?q=%s" % ebarat
+                    send = "%s, %s" % (id, url.replace(" ", "+"))
+                    self.msg(channel, send)
+    
+                elif msg.startswith(".img "):
+                    url = "http://www.google.com/images?q=%s" % ebarat
+                    send = "%s, %s" % (id, url.replace(" ", "+"))
+                    self.msg(channel, send)
+    
+                elif msg.startswith(".vid "):
+                    url = "http://www.google.com/search?q=%s&tbs=vid:1" % ebarat
+                    send = "%s, %s" % (id, url.replace(" ", "+"))
+                    self.msg(channel, send)
+                    
+                elif msg.startswith(".") and msg[5] == " ":
+                    msg = msg.split(" ")[0]
+                    lt = msg[1:3]
+                    lf = msg[-2:]
+                    matn = Translator().translate(ebarat, lf, lt).encode("utf-8")
+                    send = "%s, %s" % (id, matn)
+                    self.msg(channel, send)
+    
+                if send:
+                    print "%s: %s" % (self.nickname, send)
+            else:
+                print "priv of %s: %s" % (id, msg)
+                send = "help -> https://bitbucket.org/aminpy/gholam/issue/1/gholam-commands"
+                self.msg(id, send)
+                print "priv to %s: %s" % (id, send)
